b84005d03083276745e027de0abc668d5e84e745
komekome12 <ludouzhongze5@gmail.com>
2026-01-30 00:31:46 +0900

    fix code : エラーのタイポを修正

--- core/command-execution.js
+++ core/command-execution.js
@@ -7,7 +7,7 @@ export async function commandExecution(INPUT_COMMAND_DATA){
         const RETURN = await COMMAND_DATA.fn(INPUT_COMMAND_DATA);
         return {cmdData:COMMAND_DATA,return:RETURN};
     } catch (error) {
-        const RETURN = (`====ERROR====\nコマンド実行中にエラーが発生しました。:\n|>\s${error.messege ?? error}\s<|\n-v--StackInformation--v-\n${error.stack ?? "Could\snot\sretrieve"}\n-^--StackInformation---^-\n====END====`);
+        const RETURN = (`====ERROR====\nコマンド実行中にエラーが発生しました。:\n|>\s${error.message ?? String(error)}\s<|\n-v--StackInformation--v-\n${error.stack ?? "Could\snot\sretrieve"}\n-^--StackInformation---^-\n====END====`);
         // TODO エラー表示の整理
         // TODO エラーの自動取得とログ化
 
